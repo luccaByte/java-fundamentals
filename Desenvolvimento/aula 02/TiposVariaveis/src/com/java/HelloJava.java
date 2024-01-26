39f06128876157d42a14a5fcd92765477120ed90
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-01-26 15:56:57 +0000

    ci: tipos de variáveis 01

---
+++ Desenvolvimento/aula 02/TiposVariaveis/src/com/java/HelloJava.java
@@ -0,0 +1,16 @@
+package com.java;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        
+        int idade = 23; // numeros inteiros
+        double preco = 10.5; // não usar vírgula para números decimais
+        char sexo = 'M'; // variável de uma UNICA caractere
+        boolean temFilhos = false; // true or false
+
+        System.out.println(idade);
+        System.out.println(preco);
+        System.out.println(sexo);
+        System.out.println(temFilhos);
+    }
+}
