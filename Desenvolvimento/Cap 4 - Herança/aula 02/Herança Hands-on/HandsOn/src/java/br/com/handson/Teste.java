25a8b1f68549a98c5e878036cbd22a6804763a13
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-02-16 17:33:30 +0000

    feat: herança com hands-on

---
+++ Desenvolvimento/Cap 4 - Herança/aula 02/Herança Hands-on/HandsOn/src/java/br/com/handson/Teste.java
@@ -0,0 +1,33 @@
+package java.br.com.handson;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        
+        /* 
+            ContaCorrente cc = new ContaCorrente();
+
+            cc.setAgencia(0001);
+            cc.setNumero(0202);
+            cc.setSaldo(49.76);
+            cc.setTipo("PJ");
+            cc.setChequeEspecial(1000);
+        */
+
+        Conta conta = new Conta();
+        Conta cc = new ContaCorrente();
+
+        ContaCorrente c1 = (ContaCorrente) cc;
+        
+        if (conta instanceof ContaCorrente) {
+            ContaCorrente c2 = (ContaCorrente) conta;
+            System.out.println(c2 + "É uma conta");
+        } else {
+            System.out.println("Não é uma conta");
+        }
+
+        
+
+       
+    }
+}
