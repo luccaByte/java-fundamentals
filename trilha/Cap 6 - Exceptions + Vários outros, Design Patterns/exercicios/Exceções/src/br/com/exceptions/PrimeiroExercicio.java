397683eb2163979ecf896640e972d88e85f94864
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-04-25 18:56:06 +0000

    exercicios

---
+++ trilha/Cap 6 - Exceptions + Vários outros, Design Patterns/exercicios/Exceções/src/br/com/exceptions/PrimeiroExercicio.java
@@ -0,0 +1,12 @@
+package br.com.exceptions;
+
+public class PrimeiroExercicio {
+    public static void main(String[] args) {
+        String str = null;
+
+        // Adicione um bloco try-catch aqui para lidar com a NullPointerException
+       
+        System.out.println(str.length());
+    }
+}
+
---
+++ trilha/Cap 6 - Exceptions + Vários outros, Design Patterns/exercicios/Exceções/src/br/com/exceptions/TerceiroExercicio.java
@@ -0,0 +1,5 @@
+package br.com.exceptions;
+
+public class TerceiroExercicio {
+
+}
